33d8c22d4e92a94c2a7a4dfd65bceff728823480
Nakul003 <nakulchauhan2017@gmail.com>
2025-02-26 13:04:20 +0530

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import iconClose from "/src/assets/icon-close.svg"
 import iconHam from "/src/assets/icon-hamburger.svg"
 const Navbar = () => {
 
-  const [Left, setLeft] = useState(100)
+  const [Left, setLeft] = useState("100%")
   const [Display, setDisplay] = useState("hidden")
   const [Ham_opacity, setHam_opacity] = useState("opacity-100")
 
   const handleMenuopen=()=>{
     setTimeout(() => {
-      setLeft(0)
+      setLeft("0%")
     },0);
     document.body.style.overflow="hidden"
   }
@@ -28,8 +28,8 @@ const Navbar = () => {
         <div className='flex justify-between w-full py-[2.5rem] px-[1.5rem] items-center'>
           <img className='w-[9rem] z-20' src={logo} alt=""/>
           <img className={`${Ham_opacity}`} src={iconHam} alt="" onClick={()=>{setDisplay("flex"),setHam_opacity("opacity-0"), handleMenuopen()}} />
-          <img className={`${Display} z-20`} src={iconClose} alt="" onClick={()=>{setLeft(100),handleMenuclose()}} />
-          <ul className={`fixed pt-[13rem] ${Display} transition-all duration-300 pl-[1.5rem] font-Josefin font-light text-2xl items-start gap-4 z-10 top-0 left-[${Left}] w-screen h-screen bg-black text-white flex flex-col justify-center pb-[10rem] sm:items-center`}>
+          <img className={`${Display} z-20`} src={iconClose} alt="" onClick={()=>{setLeft("100%"),handleMenuclose()}} />
+          <ul style={{left:Left}} className={`fixed pt-[13rem] ${Display} transition-all duration-300 pl-[1.5rem] font-Josefin font-light text-2xl items-start gap-4 z-10 top-0  w-screen h-screen bg-black text-white flex flex-col justify-center pb-[10rem] sm:items-center`}>
           <li>ABOUT</li>
           <li>CAREERS</li>
           <li>EVENTS</li>
